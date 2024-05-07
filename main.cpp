b6ad44ce921c7379a405dae3a40437d64a0b3783
Alex Costa <up202207499@fe.up.pt>
2024-05-07 10:43:31 +0100

    heuristica in display menu

--- main.cpp
+++ main.cpp
@@ -4,23 +4,10 @@
 #include <chrono>
 
 int main() {
-    Graph<int> graph;
-    Data::loadConnected(&graph, "edges_25.csv");
-
-    /*DisplayMenu displayMenu = DisplayMenu();
-    displayMenu.Base();*/
-
-    auto inicio = std::chrono::high_resolution_clock::now();
-
-    /*Menu menu = Menu();
-    cout << menu.greedyHeuristica(&graph) << endl;*/
   
     DisplayMenu displayMenu = DisplayMenu();
     displayMenu.OpenMenu();
 
-    auto fim = std::chrono::high_resolution_clock::now();
-    auto duracao = std::chrono::duration_cast<std::chrono::milliseconds>(fim - inicio);
-    std::cout << "Tempo de execucao: " << duracao.count() << " milissegundos" << std::endl;
 
     return 0;
 }
