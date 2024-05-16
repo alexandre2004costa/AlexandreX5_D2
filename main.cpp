a282e581b36a0a49248a20f946ade6bba61a441f
beatrizbernardoo <beatrizxalexandra@gmail.com>
2024-05-17 00:49:53 +0100

    triangular feitooo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 
 #include "DisplayMenu.h"
+#include "Menu.h"
 
 int main() {
     DisplayMenu displayMenu = DisplayMenu();
     displayMenu.SelectGraphType();
+ /*   Menu menu=Menu();
+    cout<<menu.haversineDistance(-15.6743, -47.84923, -15.601082, -47.654252)<<endl;*/
     return 0;
 }
 
